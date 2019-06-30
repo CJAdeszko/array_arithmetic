fc7012652a35ad61925864baa4767b4eeb0a7ccc
Christopher Adeszko <cjadeszko@gmail.com>
2019-06-30 14:11:23 -0700

    Initial commit

---
+++ lib/array_arithmetic.rb
@@ -0,0 +1,226 @@
+require "array_arithmetic/version"
+
+module ArrayArithmetic
+  class Error < StandardError; end
+
+  #Return an argument as an array for use with arithmetic methods
+  def make_array(arg)
+    return [arg]
+  end
+
+  #Takes in two arrays of unequal length passed to the arithmetic method
+  #Recycles values from the shorter array
+  #Returns an array containing two arrays of equal length
+  def update_array_length(arr_one, arr_two)
+    if arr_one.length < arr_two.length
+      adjusted_arr_one = []
+      arr_two.each_with_index do |value, index|
+        if arr_one.length == 1
+          adjusted_arr_one << arr_one[0]
+        elsif index >= arr_one.length
+          adjusted_arr_one << arr_one[index - arr_one.length]
+        else
+          adjusted_arr_one << arr_one[index]
+        end
+      end
+      return adjusted_arr_one, arr_two
+    elsif arr_one.length > arr_two.length
+      adjusted_arr_two = []
+      arr_one.each_with_index do |value, index|
+        if arr_two.length == 1
+          adjusted_arr_two << arr_two[0]
+        elsif index >= arr_two.length
+          adjusted_arr_two << arr_two[index - arr_two.length]
+        else
+          adjusted_arr_two << arr_two[index]
+        end
+      end
+      return arr_one, adjusted_arr_two
+    end
+  end
+
+
+  #Adds the values of corresponding indices of two arrays
+  def add(arr_one, arr_two)
+    raise ArgumentError, 'Argument is not an array' unless arr_one.is_a? Array
+
+    result = []
+
+    if arr_two.class != Array
+      arr_two = make_array(arr_two)
+    end
+
+    if arr_one.length != arr_two.length
+      length_matched_arrays = update_array_length(arr_one, arr_two)
+
+      length_matched_arrays[0].each_with_index do |value, index|
+        result << value + length_matched_arrays[1][index]
+      end
+    else
+      arr_one.each_with_index do |value, index|
+        result << value + arr_two[index]
+      end
+    end
+
+    return result
+  end
+
+
+  #Subtracts the values of corresponding indices of two arrays
+  def subtract(arr_one, arr_two)
+    raise ArgumentError, 'Argument is not an array' unless arr_one.is_a? Array
+
+    result = []
+
+    if arr_two.class != Array
+      arr_two = make_array(arr_two)
+    end
+
+    if arr_one.length != arr_two.length
+      length_matched_arrays = update_array_length(arr_one, arr_two)
+
+      length_matched_arrays[0].each_with_index do |value, index|
+        result << value - length_matched_arrays[1][index]
+      end
+    else
+      arr_one.each_with_index do |value, index|
+        result << value - arr_two[index]
+      end
+    end
+
+    return result
+  end
+
+
+  #Multiplies the values of corresponding indices of two arrays
+  def multiply(arr_one, arr_two)
+    raise ArgumentError, 'Argument is not an array' unless arr_one.is_a? Array
+
+    result = []
+
+    if arr_two.class != Array
+      arr_two = make_array(arr_two)
+    end
+
+    if arr_one.length != arr_two.length
+      length_matched_arrays = update_array_length(arr_one, arr_two)
+
+      length_matched_arrays[0].each_with_index do |value, index|
+        result << value * length_matched_arrays[1][index]
+      end
+    else
+      arr_one.each_with_index do |value, index|
+        result << value * arr_two[index]
+      end
+    end
+
+    return result
+  end
+
+
+  #Divides the values of corresponding indices of two arrays
+  #Returns values as a float
+  def divide(arr_one, arr_two, sig_dig=2)
+    raise ArgumentError, 'Argument is not an array' unless arr_one.is_a? Array
+
+    result = []
+
+    if arr_two.class != Array
+      arr_two = make_array(arr_two)
+    end
+
+    if arr_one.length != arr_two.length
+      length_matched_arrays = update_array_length(arr_one, arr_two)
+
+      length_matched_arrays[0].each_with_index do |value, index|
+        if length_matched_arrays[1][index] == 0
+          result << "Divide by 0 error"
+        else
+          result << (value.to_f / length_matched_arrays[1][index]).round(sig_dig)
+        end
+      end
+    else
+      arr_one.each_with_index do |value, index|
+        if arr_two[index] == 0
+          result << "Divide by 0 error"
+        else
+          result << (value.to_f / arr_two[index]).round(sig_dig)
+        end
+      end
+    end
+
+    return result
+  end
+
+
+  #Calculates the remainder of corresponding inidices of two arrays
+  def remainder(arr_one, arr_two, sig_dig=2)
+    raise ArgumentError, 'Argument is not an array' unless arr_one.is_a? Array
+
+    result = []
+
+    if arr_two.class != Array
+      arr_two = make_array(arr_two)
+    end
+
+    if arr_one.length != arr_two.length
+      length_matched_arrays = update_array_length(arr_one, arr_two)
+
+      length_matched_arrays[0].each_with_index do |value, index|
+        if length_matched_arrays[1][index] == 0
+          result << "Divide by 0 error"
+        else
+          result << (value.to_f % length_matched_arrays[1][index]).round(sig_dig)
+        end
+      end
+    else
+      arr_one.each_with_index do |value, index|
+        if arr_two[index] == 0
+          result << "Divide by 0 error"
+        else
+          result << (value.to_f % arr_two[index]).round(sig_dig)
+        end
+      end
+    end
+
+    return result
+  end
+
+
+  #Raises the values of arr_one to the power of the value at corresponding indices in arr_two
+  def exponential(arr_one, arr_two)
+    raise ArgumentError, 'Argument is not an array' unless arr_one.is_a? Array
+
+    result = []
+
+    if arr_two.class != Array
+      arr_two = make_array(arr_two)
+    end
+
+    if arr_one.length != arr_two.length
+      length_matched_arrays = update_array_length(arr_one, arr_two)
+
+      length_matched_arrays[0].each_with_index do |value, index|
+        result << value ** length_matched_arrays[1][index]
+      end
+    else
+      arr_one.each_with_index do |value, index|
+        result << value ** arr_two[index]
+      end
+    end
+
+    return result
+  end
+
+
+  #Squares the values of an array
+  def square(array)
+    return exponential(array, 2)
+  end
+
+
+  #Returns the square root of each value of an array
+  def square_root(array)
+    return exponential(array, 0.5)
+  end
+end
---
+++ spec/array_arithmetic_spec.rb
@@ -0,0 +1,113 @@
+RSpec.describe ArrayArithmetic do
+  include ArrayArithmetic
+
+  it "has a version number" do
+    expect(ArrayArithmetic::VERSION).not_to be nil
+  end
+
+  describe "add" do
+    it "adds the corresponding indices of two arrays" do
+      arr_one = [1, 2, 3, 4, 5]
+      arr_two = [9, 8, 7, 6, 5]
+      result = [10, 10, 10, 10, 10]
+      expect(add(arr_one, arr_two)).to eq(result)
+    end
+
+    it "raises an error if the first argument is not an array" do
+      arr_two = [9, 8, 7, 6, 5]
+      expect{add(5, arr_two)}.to raise_error(ArgumentError, 'Argument is not an array')
+    end
+  end
+
+  describe "subtract" do
+    it "subtracts the corresponding indices of two arrays" do
+      arr_one = [1, 2, 3, 4, 5]
+      arr_two = [9, 8, 7, 6, 5]
+      result = [-8, -6, -4, -2, 0]
+      expect(subtract(arr_one, arr_two)).to eq(result)
+    end
+
+    it "raises an error if the first argument is not an array" do
+      arr_two = [9, 8, 7, 6, 5]
+      expect{subtract(5, arr_two)}.to raise_error(ArgumentError, 'Argument is not an array')
+    end
+  end
+
+  describe "multiply" do
+    it "multiplies the corresponding indices of two arrays" do
+      arr_one = [1, 2, 3, 4, 5]
+      arr_two = [9, 8, 7, 6, 5]
+      result = [9, 16, 21, 24, 25]
+      expect(multiply(arr_one, arr_two)).to eq(result)
+    end
+
+    it "raises an error if the first argument is not an array" do
+      arr_two = [9, 8, 7, 6, 5]
+      expect{multiply(5, arr_two)}.to raise_error(ArgumentError, 'Argument is not an array')
+    end
+  end
+
+  describe "divide" do
+    it "divides the corresponding indices of two arrays and returns values as a float" do
+      arr_one = [1, 2, 3, 4, 5]
+      arr_two = [2, 3, 4, 5, 6]
+      result = [0.50, 0.67, 0.75, 0.80, 0.83]
+      expect(divide(arr_one, arr_two, 2)).to eq(result)
+    end
+
+    it "raises an error if the first argument is not an array" do
+      arr_two = [9, 8, 7, 6, 5]
+      expect{divide(5, arr_two)}.to raise_error(ArgumentError, 'Argument is not an array')
+    end
+  end
+
+  describe "remainder" do
+    it "returns the remainder of corresponding indices of two arrays" do
+      arr_one = [10, 9, 8, 7, 6]
+      arr_two = [1, 2, 3, 4, 5]
+      result = [0, 1, 2, 3, 1]
+      expect(remainder(arr_one, arr_two)).to eq(result)
+    end
+
+    it "raises an error if the first argument is not an array" do
+      arr_two = [9, 8, 7, 6, 5]
+      expect{remainder(5, arr_two)}.to raise_error(ArgumentError, 'Argument is not an array')
+    end
+  end
+
+  describe "exponential" do
+    it "raises the values of the first array to the power provided" do
+      arr_one = [2, 3, 4, 5, 6]
+      result = [8, 27, 64, 125, 216]
+      expect(exponential(arr_one, 3)).to eq(result)
+    end
+
+    it "raises the values of the first array to the power of the corresponding indices of the second array" do
+      arr_one = [2, 3, 4, 5, 6]
+      arr_two = [1, 2, 3, 4, 5]
+      result = [2, 9, 64, 625, 7776]
+      expect(exponential(arr_one, arr_two)).to eq(result)
+    end
+
+    it "raises an error if the first argument is not an array" do
+      arr_two = [9, 8, 7, 6, 5]
+      expect{exponential(5, arr_two)}.to raise_error(ArgumentError, 'Argument is not an array')
+    end
+  end
+
+  describe "square" do
+    it "returns the values of the array raised to the second power" do
+      arr_one = [2, 3, 4, 5, 6]
+      result = [4, 9, 16, 25, 36]
+      expect(square(arr_one)).to eq(result)
+    end
+  end
+
+  describe "square_root" do
+    it "returns the squareroot of the values of the array" do
+      arr_one = [4, 9, 16, 25, 36]
+      result = [2, 3, 4, 5, 6]
+      expect(square_root(arr_one)).to eq(result)
+    end
+  end
+end
